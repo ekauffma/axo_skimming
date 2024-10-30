ec14afa4b15b2945a036848a0e3a0da761fda7e1
Jannicke Pearkes <jannicke.pearkes@cern.ch>
2024-10-30 15:47:03 +0100

    Syntax in longlunch

--- examine_condor_dirs.sh
+++ examine_condor_dirs.sh
@@ -1,25 +1,28 @@
+#! /bin/bash
+
 count="1"
 
-file=$1
+#file=$1
 while read -r line; do
 
-echo count $count
+printf $count
 
 dirname=$(echo $line | awk '{print $1}')
 
-echo $dirname
+printf $dirname
 
 a=($(wc $dirname/log/scriptcondor_0.err))
 lines=${a[0]}
 if [ $lines -ne 1 ]; then
 echo "Issue with condor job ${dirname}"
 cat $dirname/log/scriptcondor_0.err
-else
-rm -rf $dirname
+#else
+#rm -rf $dirname
 fi
 
 count=$[$count+1]
 
-done < $file
+done
 
 exit 0
+
