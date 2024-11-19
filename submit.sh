2fcd8442b73d6ae92b0ebecd0b4741545f54fbd1
Elliott Kauffman <elmaka8700@gmail.com>
2024-11-19 16:22:04 +0100

    edited submit.sh to check if file exists in directory

--- submit.sh
+++ submit.sh
@@ -1,5 +1,4 @@
 count="1"
-
 file=$1
 while read -r line; do
 
@@ -9,7 +8,12 @@ input_filename=$(echo $line | awk '{print $1}')
 
 echo $input_filename
 
-sh SubmitToCondor.sh output_${1}_${count} ${input_filename}
+if [ ! -f /eos/cms/store/group/phys_exotica/axol1tl/Data_ScoutingNano_withAXOscore/2024I/output_${1}_${count}.root ]; then
+	echo "File not found.\n"
+	sh SubmitToCondor.sh output_${1}_${count} ${input_filename}
+else
+	echo "File exists. Will not submit job."
+fi
 
 count=$[$count+1]
 
